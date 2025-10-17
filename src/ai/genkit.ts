61e5f9728ba4895496e4831642284c2c6077b49a
Hardik Wahi <hwahi2178@gmail.com>
2025-10-17 13:50:48 +0000

    change it back to flash please!

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,5 +3,5 @@ import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-pro-latest',
+  model: 'googleai/gemini-flash-latest',
 });
