eae7d3a1516d009df3ebb44b33d2421bc6d26c49
Hardik Wahi <hwahi2178@gmail.com>
2025-10-17 14:39:48 +0000

    add this as the logo, and also change the sidebar color to white nd rest

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,8 +12,10 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z" />
-      <path d="m9 12 2 2 4-4" />
+      <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v2" />
+      <path d="m15.5 15.5 2.5 2.5" />
+      <path d="m13 11 2 2 4-4" />
+      <circle cx="11.5" cy="11.5" r="4.5" />
     </svg>
   );
 }
